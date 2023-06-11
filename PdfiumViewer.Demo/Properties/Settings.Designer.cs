5ad703d9a2a6c922704d8ea28ecf2096adc19b1d
Yilin Yang <yyl_20050115@hotmail.com>
2023-06-12 00:22:23 +0800

    fx

--- PdfiumViewer.Demo/Properties/Settings.Designer.cs
+++ PdfiumViewer.Demo/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PdSearcher.Properties {
+namespace PdfSearcher.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
